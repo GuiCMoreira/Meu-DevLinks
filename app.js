e86d27114b7fe77cfbf4579db2178ac5d4e52300
Guilherme Carvalho <gui0307carvalho@gmail.com>
2023-11-07 14:00:29 -0300

    update

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 function toggleMode() {
-  const html = document.documentElement;
+  const html = document.documentElement
 
-  html.classList.toggle("light");
+  html.classList.toggle("light")
 
-  const img = document.querySelector("#profile img");
+  const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "assets/Avatar.png");
+    img.setAttribute("src", "assets/Avatar-light.png")
   } else {
-    img.setAttribute("src", "assets/Avatar.png");
+    img.setAttribute("src", "assets/Avatar.png")
   }
 }
